5d8bf2d5fd31dd7665339935ddf8af21458a11b3
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2020-01-27 16:51:11 -0700

    Skeleton code, nothing much

---
+++ python/sceapy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# Author: Rahul Bhadani
+# Email: rahulbhadani@email.arizona.edu
+
+class sceapy:
+    '''
+    (S)ingle (C)ell (E)stimation and (A)nalysis in (P)ython
+
+    Parameters
+    -------------
+
+
+
+    '''
